1cfe02bcc4821ea1922f81f5b1c8701f72e203a9
Jeron Aldaron Lau <aldaronlau@gmail.com>
2024-10-13 21:31:25 -0500

    Remove copyright comments for each file

--- src/pos.rs
+++ src/pos.rs
@@ -1,12 +1,3 @@
-// Copyright © 2020-2022 The Fon Contributors.
-//
-// Licensed under any of:
-// - Apache License, Version 2.0 (https://www.apache.org/licenses/LICENSE-2.0)
-// - MIT License (https://mit-license.org/)
-// - Boost Software License, Version 1.0 (https://www.boost.org/LICENSE_1_0.txt)
-// At your choosing (See accompanying files LICENSE_APACHE_2_0.txt,
-// LICENSE_MIT.txt and LICENSE_BOOST_1_0.txt).
-
 //! Speaker/channel positions within a speaker configuration.
 
 use core::ops::{Index, IndexMut};
